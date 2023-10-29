5d85a991dbca820cd708ae571ebf6fb85d7e76b1
Álvaro Fernández <alvaro_0211@hotmail.com>
2023-10-29 10:21:39 +0100

    app funcional

--- README.md
+++ README.md
@@ -1,11 +1,37 @@
-# typescript-sandbox
+# BootcampJS - Módulo 4
 
-Para la parte de cada módulo en la que te planteamos ejercicios con JavaScript puedes usar este sandbox.
+A continuación explico los pasos para que funcione este display de turnos.
 
-Pasos:
+1. He declarado la variable **turno** como `let`, dándole el valor de 0.
 
-- Clonate el proyecto.
-- Instala las dependencias con `npm install`.
-- Ejecuta el sandbox con `npm run dev`.
-- Abre el navegador en `http://localhost:5173/` (si ese puerto no te funciona, mira en la consola donde has hecho el build, puede que este ocupado y se haya abierto en otro puerto).
-- Puedes empezar a meter tu código en el fichero:
+2. Después he creado los botones necesarios en HTML y he capturado el `id` declarando las variable según lo que iba necesitando, como ejemplo:
+
+   `const botonSiguiente = document.getElementById('turnoSiguiente') as HTMLButtonElement;`
+
+3. Una vez creadas las variables que necesitaba, he creado la función actualizar turno de la siguiente manera:
+
+   `const actualizarTurno = () => turnoActual.innerText = turno.toString().padStart(2, '0');`
+
+4. El paso siguiente ha sido asignar a cada elemento HTML un `.addEventListener` y darle funcionalidad. En el caso del `botonAnterior` he hecho una comparación para que no haya turnos negativos.
+   <br/>
+   <br/>
+
+# Challenge
+
+Para conseguir que los números tengan siempre un 0 delante, al crear la función `actualizarTurno`, hago que turno pase a ser un string y luego le paso `.padStart(2, '0')`;
+<br/>
+<br/>
+
+# Avanzado
+
+1. Para empezar he creado en html los componentes necesarios y después he declarado las variables de `turnoManual` y `botonCambiarTurno`.
+
+2. Después he creado la función `cambiarTurno` con un if, que sirve para evitar que si le dan al botón de Cambiar sin haber rellenado el input, no salga NaN en el display.
+
+3. Por último he creado el evento para el botón y así se cambie manualmente.
+   <br/>
+   <br/>
+
+# Extra
+
+Haciendo un poco de búsqueda, he añadido un último evento extra, para que no se puedan añadir al input números negativos ni decimales. Lo he hecho un poco por evitar posibles errores.
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,135 @@
+// document.addEventListener(`DOMContentLoaded`, muestraPuntuacion);
 import "./style.css";
 
-console.log("Hello Typescript!");
+// Mostrar Puntuacion
+let puntuacionUsuario: number = 0;
+const elementoPuntuacion = document.getElementById('puntuacion');
+
+const muestraPuntuacion = (carta: number) => {
+
+    if (carta >= 10) {
+        carta = 0.5;
+    }
+
+    puntuacionUsuario = puntuacionUsuario + carta;
+
+    if (elementoPuntuacion) {
+        elementoPuntuacion.innerHTML = `Tu puntuación es ${puntuacionUsuario}`;
+        // Game Over
+        const gameOver = (puntuacionUsuario: number) => {
+            if (puntuacionUsuario > 7.5 && botonDarCarta instanceof HTMLButtonElement) {
+                elementoPuntuacion.innerHTML = 'Game over';
+                botonDarCarta.disabled = true;
+            }
+        }
+        gameOver(puntuacionUsuario);
+
+    }
+}
+
+
+// Pedir carta
+const dameCarta = () => {
+
+    let carta = Math.floor(Math.random() * 10 + 1);
+    if (carta > 7) {
+        carta = carta + 2;
+    }
+    muestraCarta(carta);
+    muestraPuntuacion(carta);
+};
+
+// Reset
+const reset = () => {
+    if (botonDarCarta instanceof HTMLButtonElement) {
+        botonDarCarta.disabled = false;
+        if (elementoPuntuacion) {
+            puntuacionUsuario = 0;
+            elementoPuntuacion.innerHTML = '';
+            const cartaBack = '/src/images/back.jpg';
+            elementoImagenCarta.src = cartaBack;
+        }
+    }
+}
+
+// Plantarse
+const plantarse = () => {
+    if (elementoPuntuacion) {
+        if (puntuacionUsuario >= 0.5 && puntuacionUsuario <= 4) {
+            let mensaje = 'Has sido muy conservador';
+            elementoPuntuacion.innerHTML = mensaje;
+        }
+        if (puntuacionUsuario === 5) {
+            elementoPuntuacion.innerHTML = 'Te ha entrado el canguelo eh?';
+        }
+        if (puntuacionUsuario === 6 || puntuacionUsuario === 7) {
+            elementoPuntuacion.innerHTML = 'Casi casi...';
+        }
+        if (puntuacionUsuario === 7.5) {
+            elementoPuntuacion.innerHTML = '¡Lo has clavado! ¡Enhorabuena!';
+        }
+    }
+}
+
+// Mostrar Carta
+let elementoImagenCarta = document.getElementById('imagen') as HTMLImageElement;
+
+const muestraCarta = (carta: number): void => {
+
+    switch (carta) {
+        case carta = 1:
+            const asCopas = '/src/images/1_as-copas.jpg';
+            elementoImagenCarta.src = asCopas;
+            break;
+        case carta = 2:
+            const dosCopas = '/src/images/2_dos-copas.jpg';
+            elementoImagenCarta.src = dosCopas;
+            break;
+        case carta = 3:
+            const tresCopas = '/src/images/3_tres-copas.jpg';
+            elementoImagenCarta.src = tresCopas;
+            break;
+        case carta = 4:
+            const cuatroCopas = '/src/images/4_cuatro-copas.jpg';
+            elementoImagenCarta.src = cuatroCopas;
+            break;
+        case carta = 5:
+            const cincoCopas = '/src/images/5_cinco-copas.jpg';
+            elementoImagenCarta.src = cincoCopas;
+            break;
+        case carta = 6:
+            const seisCopas = '/src/images/6_seis-copas.jpg';
+            elementoImagenCarta.src = seisCopas;
+            break;
+        case carta = 7:
+            const sieteCopas = '/src/images/7_siete-copas.jpg';
+            elementoImagenCarta.src = sieteCopas;
+            break;
+        case carta = 10:
+            const sotaCopas = '/src/images/10_sota-copas.jpg';
+            elementoImagenCarta.src = sotaCopas;
+            break;
+        case carta = 11:
+            const caballoCopas = '/src/images/11_caballo-copas.jpg';
+            elementoImagenCarta.src = caballoCopas;
+            break;
+        case carta = 12:
+            const reyCopas = '/src/images/12_rey-copas.jpg';
+            elementoImagenCarta.src = reyCopas;
+            break;
+
+    };
+}
+
+
+// Botón 'Dar Carta'
+const botonDarCarta = document.getElementById('pideCarta');
+botonDarCarta?.addEventListener('click', dameCarta);
+
+// Botón 'Volver a jugar'
+const botonReset = document.getElementById('reset');
+botonReset?.addEventListener('click', reset);
+
+// Botón 'Plantarse'
+const botonPlantarse = document.getElementById('mePlanto');
+botonPlantarse?.addEventListener('click', plantarse);
