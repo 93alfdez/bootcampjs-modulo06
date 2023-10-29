ffbdf7df6b8ef3811496dbd9dc51d2d6c3e5f4ed
Álvaro Fernández <alvaro_0211@hotmail.com>
2023-10-29 17:05:40 +0100

    laboratorio terminado

--- README.md
+++ README.md
@@ -1,37 +1,105 @@
-# BootcampJS - Módulo 4
+# BootcampJS - Módulo 5
 
-A continuación explico los pasos para que funcione este display de turnos.
+A continuación dejo la explicación de como he creado el juego:
 
-1. He declarado la variable **turno** como `let`, dándole el valor de 0.
+He declarado tres variables `let` globales como son `puntuacionUsuario`, `carta` y `cartasMostradas`, que más adelante las explico.
 
-2. Después he creado los botones necesarios en HTML y he capturado el `id` declarando las variable según lo que iba necesitando, como ejemplo:
+He capturado los div de `puntuacion`, `mensaje` e `imagen` con `getElementById`. También los botones de `botonDarCarta`, `botonReset`, `botonPlantarse`, `botonQuePasaria`.
 
-   `const botonSiguiente = document.getElementById('turnoSiguiente') as HTMLButtonElement;`
+También he separado la puntuación de los mensajes que aparecen cuando el usuario hace clic en el botón de _Me planto_ o _¿Quieres saber qué carta era la siguiente?_ para que siempre aparezca la información de la puntuación cuando está jugando o ha terminado el juego. Aclarar que antes de empezar no aparece que la puntuación es 0.
 
-3. Una vez creadas las variables que necesitaba, he creado la función actualizar turno de la siguiente manera:
+1. **Mostrar puntuación**
 
-   `const actualizarTurno = () => turnoActual.innerText = turno.toString().padStart(2, '0');`
+   He creado la variable `let puntuacionUsuario: number = 0;` para almacenar la puntuación del usuario.
 
-4. El paso siguiente ha sido asignar a cada elemento HTML un `.addEventListener` y darle funcionalidad. En el caso del `botonAnterior` he hecho una comparación para que no haya turnos negativos.
+   En el html he metido un div con el `id=puntuacion`. Declaré la variable `elementoPuntuacion` para recoger el id del div.
+
+   - Hice la función `muestraPuntuacion`:
+
+     Hay un `if` para que se muestre en el div de `puntuacion` que he capturado al principio la puntuación con una frase.
+
+     También habia invocado la función para poder verla en el DOM con `document.addEventListener(`DOMContentLoaded`, muestraPuntuacion);`. Lo dejo comentado porque luego ya no es necesaria.
+     <br/>
+     <br/>
+
+2. **Pedir carta**
+
+   Creo la función `dameCarta` en la cual digo, que la variable `carta` antes creada como `let` y disponible en el ámbito global utilice los métodos `Math.floor(Math.random() * 10 + 1)` para que la puntuación sea un valor aleatorio entre 0 y 10, le sumamos uno para que nunca sea cero porque esa carta no existe.
+
+   Pasamos a un `if` en el que si la `carta > 7` le sume 2, para saltarnos los valores 8 y 9 de la baraja, y así obtener el resto de valores faltantes.
+
+   De manera adicional, utilizando la variable `cartasMostradas` que es un array vacio de números, y un `if` utilizando el método `.includes()` pasando el valor de la carta y después `cartasMostradas.push(carta)`, añado al final del array las cartas que se han utilizado para no volver a pasarlas.
    <br/>
    <br/>
 
-# Challenge
+3. **Mostrar carta**
 
-Para conseguir que los números tengan siempre un 0 delante, al crear la función `actualizarTurno`, hago que turno pase a ser un string y luego le paso `.padStart(2, '0')`;
-<br/>
-<br/>
+   A partir de la función recomendada que se facilita en el ejercicio, creo un switch con todos los casos posibles según el valor de la carta y la imagen que le corresponde.
 
-# Avanzado
+   También he creado un elemento `<img>` en el index, para poder cambiar el src. De manera predeterminada tiene la imagen trasera de la carta.
 
-1. Para empezar he creado en html los componentes necesarios y después he declarado las variables de `turnoManual` y `botonCambiarTurno`.
-
-2. Después he creado la función `cambiarTurno` con un if, que sirve para evitar que si le dan al botón de Cambiar sin haber rellenado el input, no salga NaN en el display.
-
-3. Por último he creado el evento para el botón y así se cambie manualmente.
+   En el `.addEventListener` del `botonDarCarta` llamo a las funciones `dameCarta()`,`muestraCarta(carta)` y `muestraPuntuacion(carta)` para que se ejecuten al hacer clic.
    <br/>
    <br/>
 
-# Extra
+4. **Sumar Puntuación**
 
-Haciendo un poco de búsqueda, he añadido un último evento extra, para que no se puedan añadir al input números negativos ni decimales. Lo he hecho un poco por evitar posibles errores.
+   En la función `muestraPuntuacion` hay un if en el cual le digo que la carta que tenga un valor mayor o igual que 10, tenga un valor de 0.5.
+
+   A continuación se lo sumo a la puntuación del usuario, para que sume el valor al resto de cartas que hayan salido con valor menor de 10 y así aparezca en el div de manera correcta.
+   <br/>
+   <br/>
+
+5. **Game Over**
+
+   Primero he creado la funcion `gameOver` pásandole la puntuación del usuario y mediante un `if` en el que le digo que añada al `elementoPuntuacion` y al `elementoMensaje` sus respectivos mensajes, en este caso la puntuación y _Game over_.
+
+   Además, en otro `if` deshabilito los botones de _Pide Carta_ y _Me planto_ para que no se puedan utilizar.
+   <br/>
+   <br/>
+
+6. **Me planto**
+
+   En el HTML añado un botón de _Me planto_ con su id `mePlanto`.
+
+   En la función `plantarse` creo un if en el que añado cada uno de los mensajes solicitados.
+
+   Cuando el usuario tiene como resultado 7.5, también deshabilito los botones de _Pide Carta_ y _Me planto_ para que no se puedan utilizar.
+
+   En el `.addEventListener` del `botonPlantarse` llamo a la función `plantarse()` para que se ejecute al hacer clic.
+   <br/>
+   <br/>
+
+7. **Nueva Partida**
+   En el HTML añado un botón de _Volver a jugar_ con su id `reset`.
+
+   Dentro de la función `reset` creo dos `if`.
+
+   El primero de ellos deshabilito los botones _Pide Carta_, _Me planto_ y oculto el que creo más adelante de _¿Quieres saber qué carta era la siguiente?_.
+
+   En el segundo, devuelvo la puntuación a 0, los div de puntuación y mensaje, vuelve a estar vacíos, la carta vuelve a ser la parte trasera y el array de `cartasMostradas` vuelve a estar vacío.
+   <br/>
+   <br/>
+
+8. **Estila la aplicación**
+
+   - Le he añadido un fondo de tapete.
+
+   - He cambiado la tipografía global por _Urbanist_.
+
+   - Colores primarios y hover de los botones cambiados.
+
+   - He retocado las cartas para que no tengan fondo blanco y quede un poco más real.
+     <br/>
+     <br/>
+
+9. **Apartado opcional | Saber lo que habría pasado**
+   Creo un botón nuevo en HTML, que se llama _¿Quieres saber qué carta era la siguiente?_ con el id `quePasaria`.
+
+   En la función `quePasaria` creo un `if` en el que le digo que si `puntuacionUsuario >= 0.5 && puntuacionUsuario <= 7` muestre el botón y deshabilite los de _Pide Carta_ y _Me planto_.
+
+   En el `.addEventListener` del `quePasaria` llamo a las funciones `dameCarta()`,`muestraCarta(carta)` y `muestraPuntuacion(carta)` para que se ejecuten al hacer clic, además de un `if` para mostrar la puntuación que hubiese tenido el jugador y deshabilitando el botón de _¿Quieres saber qué carta era la siguiente?_.
+
+   En el `.addEventListener` del `botonPlantarse` añado la función `quePasaria()` para que se ejecute al hacer clic y aparezca el botón que he creado cuando el usuario se planta.
+   <br/>
+   <br/>
--- src/main.ts
+++ src/main.ts
@@ -1,103 +1,90 @@
-// document.addEventListener(`DOMContentLoaded`, muestraPuntuacion);
-import "./style.css";
-
-// Mostrar Puntuacion
+// Variables y elementos
 let puntuacionUsuario: number = 0;
 let carta: number;
+let cartasMostradas: number[] = [];
+
+
 const elementoPuntuacion = document.getElementById('puntuacion');
 const elementoMensaje = document.getElementById('mensaje');
+let elementoImagenCarta = document.getElementById('imagen') as HTMLImageElement;
 
+
+// Botón 'Dar Carta'
+const botonDarCarta = document.getElementById('pideCarta');
+botonDarCarta?.addEventListener('click', () => {
+    dameCarta();
+    muestraCarta(carta);
+    muestraPuntuacion(carta);
+});
+
+// Botón 'Volver a jugar'
+const botonReset = document.getElementById('reset');
+botonReset?.addEventListener('click', () => {
+    reset();
+    if (botonQuePasaria instanceof HTMLButtonElement) {
+        botonQuePasaria.disabled = false;
+        botonQuePasaria.classList.remove('mostrar');
+    };
+});
+
+// Botón 'Plantarse'
+const botonPlantarse = document.getElementById('mePlanto');
+botonPlantarse?.addEventListener('click', () => {
+    plantarse();
+    quePasaria();
+});
+
+// Botón 'Que pasaría'
+const botonQuePasaria = document.getElementById('quePasaria');
+botonQuePasaria?.addEventListener('click', () => {
+    dameCarta();
+    muestraPuntuacion(carta);
+    muestraCarta(carta);
+    if (elementoPuntuacion && elementoMensaje && botonQuePasaria instanceof HTMLButtonElement) {
+        elementoPuntuacion.innerHTML = `Tu puntuación hubiese sido de ${puntuacionUsuario}`;
+        elementoMensaje.innerHTML = '';
+        botonQuePasaria.disabled = true;
+    };
+});
+
+// Mostrar Puntuacion
 const muestraPuntuacion = (carta: number) => {
-
     if (carta >= 10) {
         carta = 0.5;
-    }
+    };
 
     puntuacionUsuario = puntuacionUsuario + carta;
 
     if (elementoPuntuacion && elementoMensaje) {
         elementoPuntuacion.innerHTML = `Tu puntuación es ${puntuacionUsuario}`;
-        // Game Over
-        const gameOver = (puntuacionUsuario: number) => {
-            if (puntuacionUsuario > 7.5 && botonDarCarta instanceof HTMLButtonElement && botonPlantarse instanceof HTMLButtonElement) {
-                elementoPuntuacion.innerHTML = `Tu puntuación es ${puntuacionUsuario}<br/>Game over`;
-                botonDarCarta.disabled = true;
-                botonPlantarse.disabled = true;
-            }
-        }
-        gameOver(puntuacionUsuario);
+    };
 
-    }
+    if (puntuacionUsuario > 7.5) {
+        gameOver(puntuacionUsuario);
+    };
 }
 
+// document.addEventListener(`DOMContentLoaded`, muestraPuntuacion);
 
 // Pedir carta
-
-let cartasMostradas: number[] = [];
-
 const dameCarta = () => {
 
     carta = Math.floor(Math.random() * 10 + 1);
 
     if (carta > 7) {
         carta = carta + 2;
-    }
+    };
 
     if (cartasMostradas.includes(carta)) {
         dameCarta();
         return;
-    }
+    };
 
     cartasMostradas.push(carta);
-
-    muestraCarta(carta);
-    muestraPuntuacion(carta);
 };
 
-// Reset
-const reset = () => {
-    if (botonDarCarta instanceof HTMLButtonElement && botonPlantarse instanceof HTMLButtonElement) {
-        botonDarCarta.disabled = false;
-        botonPlantarse.disabled = false;
-        if (elementoPuntuacion && elementoMensaje) {
-            puntuacionUsuario = 0;
-            elementoPuntuacion.innerHTML = '';
-            elementoMensaje.innerHTML = '';
-            elementoImagenCarta.src = '/src/images/back.png';
-            cartasMostradas = [];
-        }
-    }
-}
-
-// Plantarse
-const plantarse = () => {
-    if (elementoMensaje && elementoPuntuacion) {
-        if (puntuacionUsuario >= 0.5 && puntuacionUsuario <= 4) {
-            elementoPuntuacion.innerHTML = `Tu puntuación es ${puntuacionUsuario}`;
-            elementoMensaje.innerHTML = `Has sido muy conservador`;
-        }
-        else if (puntuacionUsuario === 5) {
-            elementoPuntuacion.innerHTML = `Tu puntuación es ${puntuacionUsuario}`;
-            elementoMensaje.innerHTML = `Te ha entrado el canguelo eh?`;
-        }
-        else if (puntuacionUsuario >= 6 || puntuacionUsuario === 7) {
-            elementoPuntuacion.innerHTML = `Tu puntuación es ${puntuacionUsuario}`;
-            elementoMensaje.innerHTML = `Casi casi...`;
-        }
-        else if (puntuacionUsuario === 7.5 && botonDarCarta instanceof HTMLButtonElement && botonPlantarse instanceof HTMLButtonElement) {
-            elementoPuntuacion.innerHTML = `Tu puntuación es ${puntuacionUsuario}<br/>`;
-            elementoMensaje.innerHTML = `¡Lo has clavado! ¡Enhorabuena!`;
-            botonDarCarta.disabled = true;
-            botonPlantarse.disabled = true;
-        }
-    }
-}
-
 // Mostrar Carta
-let elementoImagenCarta = document.getElementById('imagen') as HTMLImageElement;
-
 const muestraCarta = (carta: number): void => {
-
     switch (carta) {
         case carta = 1:
             const asCopas = '/src/images/1_as-copas.png';
@@ -139,19 +126,74 @@ const muestraCarta = (carta: number): void => {
             const reyCopas = '/src/images/12_rey-copas.png';
             elementoImagenCarta.src = reyCopas;
             break;
-
     };
+};
+
+// Game Over
+const gameOver = (puntuacionUsuario: number) => {
+
+    if (elementoPuntuacion && elementoMensaje) {
+        elementoPuntuacion.innerHTML = `Tu puntuación es ${puntuacionUsuario}`;
+        elementoMensaje.innerHTML = 'Game over';
+    }
+
+    if (botonDarCarta instanceof HTMLButtonElement && botonPlantarse instanceof HTMLButtonElement) {
+        botonDarCarta.disabled = true;
+        botonPlantarse.disabled = true;
+    }
 }
 
+// Me Planto
+const plantarse = () => {
+    if (elementoMensaje && elementoPuntuacion) {
+        if (puntuacionUsuario >= 0.5 && puntuacionUsuario <= 4) {
+            elementoPuntuacion.innerHTML = `Tu puntuación es ${puntuacionUsuario}`;
+            elementoMensaje.innerHTML = `Has sido muy conservador`;
+        }
+        else if (puntuacionUsuario === 5) {
+            elementoPuntuacion.innerHTML = `Tu puntuación es ${puntuacionUsuario}`;
+            elementoMensaje.innerHTML = `Te ha entrado el canguelo eh?`;
+        }
+        else if (puntuacionUsuario >= 6 || puntuacionUsuario === 7) {
+            elementoPuntuacion.innerHTML = `Tu puntuación es ${puntuacionUsuario}`;
+            elementoMensaje.innerHTML = `Casi casi...`;
+        }
+        else if (puntuacionUsuario === 7.5 && botonDarCarta instanceof HTMLButtonElement && botonPlantarse instanceof HTMLButtonElement) {
+            elementoPuntuacion.innerHTML = `Tu puntuación es ${puntuacionUsuario}`;
+            elementoMensaje.innerHTML = `¡Lo has clavado! ¡Enhorabuena!`;
+            botonDarCarta.disabled = true;
+            botonPlantarse.disabled = true;
+        }
+    }
+};
 
-// Botón 'Dar Carta'
-const botonDarCarta = document.getElementById('pideCarta');
-botonDarCarta?.addEventListener('click', dameCarta);
 
-// Botón 'Volver a jugar'
-const botonReset = document.getElementById('reset');
-botonReset?.addEventListener('click', reset);
+// Nueva Partida
+const reset = () => {
 
-// Botón 'Plantarse'
-const botonPlantarse = document.getElementById('mePlanto');
-botonPlantarse?.addEventListener('click', plantarse);
+    if (botonDarCarta instanceof HTMLButtonElement && botonPlantarse instanceof HTMLButtonElement && botonQuePasaria instanceof HTMLButtonElement) {
+        botonDarCarta.disabled = false;
+        botonPlantarse.disabled = false;
+        botonQuePasaria.classList.remove('mostrar');
+    };
+
+    if (elementoPuntuacion && elementoMensaje) {
+        puntuacionUsuario = 0;
+        elementoPuntuacion.innerHTML = '';
+        elementoMensaje.innerHTML = '';
+        elementoImagenCarta.src = '/src/images/back.png';
+        cartasMostradas = [];
+    };
+};
+
+// Qué hubiese pasado
+const quePasaria = () => {
+    if (puntuacionUsuario >= 0.5 && puntuacionUsuario <= 7) {
+        if (botonQuePasaria instanceof HTMLButtonElement && botonDarCarta instanceof HTMLButtonElement && botonPlantarse instanceof HTMLButtonElement) {
+            botonQuePasaria.className = 'mostrar';
+            botonDarCarta.disabled = true;
+            botonPlantarse.disabled = true;
+        }
+    }
+};
+
